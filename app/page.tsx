cc62c4815c2566f44393055071ad5026bb89afa7
Ethan Shaw <ethanashaw@icloud.com>
2024-02-25 07:01:16 -0700

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { Button, ChakraProvider, Grid, GridItem, Text } from '@chakra-ui/react'
+import { useState } from 'react'
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+class Operand {
+	static #currentOperand;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	static get current() {
+		return this.currentOperand
+	}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	static set current(operand) {
+		this.currentOperand = operand
+		operand.canOverwrite = true
+	}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	constructor() {
+		this.reset()
+	}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	reset() {
+		this.value = 0
+		this.canOverwrite = false
+	}
+}
+
+enum Operation { Add, Subtract, Multiply, Divide }
+
+let operands = [new Operand(), new Operand()]
+let operation = Operation.Add
+Operand.current = operands[0]
+
+export default function() {
+	const [display, setDisplay] = useState('0')
+	const [shouldClearAll, setShouldClearAll] = useState(true)
+
+	function setCurrentValue(value) {
+		Operand.current.value = value
+		setDisplay(Operand.current.value)
+	}
+
+	function resetDisplay(value) {
+		Operand.current = operands[0]
+		setCurrentValue(value)
+	}
+
+	function GridButton({ children, colSpan, onClick, symbol }) {
+		return (
+			<GridItem colSpan={colSpan}>
+				<Button style={{ width: '100%' }} onClick={onClick}>{symbol}</Button>
+			</GridItem>
+		)
+	}
+
+	function NumberGridButton({ colSpan, number }) {
+		return (
+			<GridButton onClick={() => {
+				if (Operand.current.canOverwrite) {
+					Operand.current.reset()
+				}
+
+				setCurrentValue(Operand.current.value * 10 + number)
+				setShouldClearAll(false)
+			}} symbol={number} colSpan={colSpan} />
+		)
+	}
+
+	function OperationGridButton({ operation: newOperation, symbol }) {
+		return (
+			<GridButton onClick={() => {
+				operation = newOperation
+				Operand.current = operands[1]
+			}} symbol={symbol} />
+		)
+	}
+
+	function ValueGridButton({ value, symbol }) {
+		return (
+			<GridButton onClick={() => setCurrentValue(value)} symbol={symbol} />
+		)
+	}
+
+	return (
+		<ChakraProvider>
+			<Text style={{ fontSize: '200%', textAlign: 'right' }}>{display}</Text>
+
+			<Grid templateColumns='repeat(4, 1fr)'>
+				<GridButton onClick={() => {
+					if (shouldClearAll) {
+						operands[1].reset()
+						operation = Operation.Add
+						resetDisplay(0)
+					} else {
+						setCurrentValue(0)
+						setShouldClearAll(true)
+					}
+				}} symbol={shouldClearAll ? 'AC' : 'C'} />
+
+				<ValueGridButton value={-Operand.current.value} symbol='±' />
+				<ValueGridButton value={Operand.current.value / 100} symbol='%' />
+				<OperationGridButton operation={Operation.Divide} symbol='÷' />
+				<NumberGridButton number={7} />
+				<NumberGridButton number={8} />
+				<NumberGridButton number={9} />
+				<OperationGridButton operation={Operation.Multiply} symbol='×' />
+				<NumberGridButton number={4} />
+				<NumberGridButton number={5} />
+				<NumberGridButton number={6} />
+				<OperationGridButton operation={Operation.Add} symbol='+' />
+				<NumberGridButton number={1} />
+				<NumberGridButton number={2} />
+				<NumberGridButton number={3} />
+				<OperationGridButton operation={Operation.Subtract} symbol='−' />
+				<NumberGridButton number={0} colSpan={2} />
+				<GridButton symbol='.' />
+
+				<GridButton onClick={() => resetDisplay((() => {
+					switch (operation) {
+					case Operation.Add: return operands[0].value + operands[1].value
+					case Operation.Subtract: return operands[0].value - operands[1].value
+					case Operation.Multiply: return operands[0].value * operands[1].value
+					case Operation.Divide: return operands[0].value / operands[1].value
+					} }
+				)())} symbol='=' />
+			</Grid>
+		</ChakraProvider>
+	)
 }
