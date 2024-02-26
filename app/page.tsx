01c6f783b02906fed2dba2ebdffbd70790697c31
Ethan Shaw <ethanashaw@icloud.com>
2024-02-25 17:10:21 -0700

    Fix nested components

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,42 @@ let operands = [new Operand(), new Operand()]
 let operation = Operation.Add
 Operand.current = operands[0]
 
+function GridButton({ colSpan, onClick, symbol }) {
+	return (
+		<GridItem colSpan={colSpan}>
+			<Button style={{ width: '100%' }} onClick={onClick}>{symbol}</Button>
+		</GridItem>
+	)
+}
+
+function NumberGridButton({ colSpan, number, setCurrentValue, setShouldClearAll }) {
+	return (
+		<GridButton onClick={() => {
+			if (Operand.current.canOverwrite) {
+				Operand.current.reset()
+			}
+
+			setCurrentValue(Operand.current.value * 10 + number)
+			setShouldClearAll(false)
+		}} symbol={number} colSpan={colSpan} />
+	)
+}
+
+function OperationGridButton({ operation: newOperation, symbol }) {
+	return (
+		<GridButton onClick={() => {
+			operation = newOperation
+			Operand.current = operands[1]
+		}} symbol={symbol} />
+	)
+}
+
+function ValueGridButton({ value, symbol, setCurrentValue }) {
+	return (
+		<GridButton onClick={() => setCurrentValue(value)} symbol={symbol} />
+	)
+}
+
 export default function() {
 	const [display, setDisplay] = useState('0')
 	const [shouldClearAll, setShouldClearAll] = useState(true)
@@ -43,42 +79,6 @@ export default function() {
 	function resetDisplay(value) {
 		Operand.current = operands[0]
 		setCurrentValue(value)
-	}
-
-	function GridButton({ children, colSpan, onClick, symbol }) {
-		return (
-			<GridItem colSpan={colSpan}>
-				<Button style={{ width: '100%' }} onClick={onClick}>{symbol}</Button>
-			</GridItem>
-		)
-	}
-
-	function NumberGridButton({ colSpan, number }) {
-		return (
-			<GridButton onClick={() => {
-				if (Operand.current.canOverwrite) {
-					Operand.current.reset()
-				}
-
-				setCurrentValue(Operand.current.value * 10 + number)
-				setShouldClearAll(false)
-			}} symbol={number} colSpan={colSpan} />
-		)
-	}
-
-	function OperationGridButton({ operation: newOperation, symbol }) {
-		return (
-			<GridButton onClick={() => {
-				operation = newOperation
-				Operand.current = operands[1]
-			}} symbol={symbol} />
-		)
-	}
-
-	function ValueGridButton({ value, symbol }) {
-		return (
-			<GridButton onClick={() => setCurrentValue(value)} symbol={symbol} />
-		)
 	}
 
 	return (
@@ -97,22 +97,22 @@ export default function() {
 					}
 				}} symbol={shouldClearAll ? 'AC' : 'C'} />
 
-				<ValueGridButton value={-Operand.current.value} symbol='±' />
-				<ValueGridButton value={Operand.current.value / 100} symbol='%' />
+				<ValueGridButton value={-Operand.current.value} symbol='±' setCurrentValue={setCurrentValue} />
+				<ValueGridButton value={Operand.current.value / 100} symbol='%' setCurrentValue={setCurrentValue} />
 				<OperationGridButton operation={Operation.Divide} symbol='÷' />
-				<NumberGridButton number={7} />
-				<NumberGridButton number={8} />
-				<NumberGridButton number={9} />
+				<NumberGridButton number={7} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={8} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={9} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
 				<OperationGridButton operation={Operation.Multiply} symbol='×' />
-				<NumberGridButton number={4} />
-				<NumberGridButton number={5} />
-				<NumberGridButton number={6} />
+				<NumberGridButton number={4} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={5} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={6} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
 				<OperationGridButton operation={Operation.Add} symbol='+' />
-				<NumberGridButton number={1} />
-				<NumberGridButton number={2} />
-				<NumberGridButton number={3} />
+				<NumberGridButton number={1} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={2} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
+				<NumberGridButton number={3} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
 				<OperationGridButton operation={Operation.Subtract} symbol='−' />
-				<NumberGridButton number={0} colSpan={2} />
+				<NumberGridButton number={0} colSpan={2} setCurrentValue={setCurrentValue} setShouldClearAll={setShouldClearAll} />
 				<GridButton symbol='.' />
 
 				<GridButton onClick={() => resetDisplay((() => {
