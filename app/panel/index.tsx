31671cc6d41d63cea1117b4bd248fd9fbf24a0ba
Ethan Shaw <ethanashaw@icloud.com>
2024-03-12 17:35:45 -0700

    Fix paper tape scroll bug

--- app/panel/index.tsx
+++ app/panel/index.tsx
@@ -1,8 +1,7 @@
-import { ReactNode } from 'react'
-import css from './index.module.css'
+import style from './style.module.css'
 
-export default function Panel({ name, children }: { name: string, children: ReactNode } ) {
-	return <div id={css.root}>
+export default function Panel({ children, name, ...props }: JSX.IntrinsicElements['div'] & { name: string } ) {
+	return <div {...props} id={style.root}>
 		<h1>
 			{name}
 		</h1>
--- app/paper-tape/index.tsx
+++ app/paper-tape/index.tsx
@@ -1,17 +1,18 @@
 import { Fragment, RefObject, useEffect, useRef } from 'react'
 import Panel from '../panel'
-import { PaperTapeEntry, operations } from '../utils'
+import operations from '../operations'
+import { KeyedPaperTapeEntry } from '../utils'
 
-export default function PaperTape({ history, clearHistory }: { history: PaperTapeEntry[], clearHistory: () => void }) {
+export default function PaperTape({ entries, clear }: { entries: KeyedPaperTapeEntry[], clear: () => void }) {
 	const paperTapeRef = useRef<Element>(null!)
 
 	useEffect(() => {
 		paperTapeRef.current.scrollIntoView()
-	}, [history])
+	}, [entries])
 
 	return <Panel name='Paper Tape'>
 		<div style={{ overflowY: 'scroll', height: '200px' }}>
-			{history.map((entry) => {
+			{entries.map((entry) => {
 				return <Fragment key={entry.key}>
 					{`${entry.operands[0]} ${operations[entry.operationId].symbolASCII} ${entry.operands[1]}`}
 					<br />
@@ -24,7 +25,7 @@ export default function PaperTape({ history, clearHistory }: { history: PaperTap
 			<div ref={paperTapeRef as RefObject<HTMLDivElement>} />
 		</div>
 
-		<button style={{ display: 'block', marginLeft: 'auto' }} onClick={clearHistory}>
+		<button style={{ display: 'block', marginLeft: 'auto' }} onClick={clear}>
 			Clear
 		</button>
 	</Panel>
